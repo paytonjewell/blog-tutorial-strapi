7f671b1037c8eecf39a86005e77f649b9f208059
Payton Jewell <payton@mathisonprojects.com>
2022-06-09 12:53:18 -0500

    initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,13 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'global::flatten-response',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
